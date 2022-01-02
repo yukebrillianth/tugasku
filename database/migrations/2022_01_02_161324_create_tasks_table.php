98a692fe54c2fa945ff8a22f665e7d474465d98f
yuke brilliant <yukebrilliant@gmail.com>
2022-01-02 23:14:27 +0700

    Feat: add Task migration & model

---
+++ database/migrations/2022_01_02_161324_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename')->nullable(false);
+            $table->string('path')->nullable(false);
+            $table->string('fileformat');
+            $table->string('filesize');
+            $table->foreignId('assignment_id')->constrained('assignments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
