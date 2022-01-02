179eba9a71d45b5ffc56b8ab17c9f1096c0ce838
yuke brilliant <yukebrilliant@gmail.com>
2022-01-02 21:32:42 +0700

    Feat: add subjects migration

---
+++ database/migrations/2022_01_02_143204_create_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->foreignId('teacher_id')
+                ->constrained('teachers')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
