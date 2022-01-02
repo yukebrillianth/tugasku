77bc16403b781f215895a30a681c67c7004ffddf
yuke brilliant <yukebrilliant@gmail.com>
2022-01-02 21:57:15 +0700

    Feat: add assignment_attachment migration & model

---
+++ database/migrations/2022_01_02_144517_create_assignment_attachments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignmentAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignment_attachments', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename')->nullable(false);
+            $table->string('path')->nullable(false);
+            $table->foreignId('assignment_id')->constrained('assignments');
+            $table->string('fileformat');
+            $table->string('filesize');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignment_attachments');
+    }
+}
